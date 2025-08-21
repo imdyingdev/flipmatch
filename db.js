b940578d7d378938cce73fba333370afd09e5a43
virgocerco <mondegrafe@gmail.com>
2025-08-22 00:16:42 +0800

    fix: Use Cloud SQL private connection instead of public IP

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const postgres = require('postgres');
 
 const sql = postgres({
-    host: process.env.PGHOST || '34.9.200.153',
-    port: process.env.PGPORT || 5432,
+    host: process.env.INSTANCE_CONNECTION_NAME ? `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}` : (process.env.PGHOST || '34.9.200.153'),
+    port: process.env.INSTANCE_CONNECTION_NAME ? undefined : (process.env.PGPORT || 5432),
     database: process.env.PGDATABASE || 'postgres',
     username: process.env.PGUSER || 'postgres',
     password: process.env.PGPASSWORD || 'JeaGrafe2004*',
